419586e88a55db557a1407b1be1474de19fba725
Rishikesh Rahi <rishikesh.rahi@tpcentralodisha.com>
2024-04-20 01:47:53 +0530

    initial commit

---
+++ android-demo-lib/src/main/java/com/tpcodl/android_demo_lib/TestRahi.java
@@ -0,0 +1,14 @@
+package com.tpcodl.android_demo_lib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestRahi {
+
+    public static void main(String[] arr){
+
+    }
+    public static void showMessage(Context context){
+        Toast.makeText(context, "Show toast", Toast.LENGTH_SHORT).show();
+    }
+}
